1c9e98446d9a30fb2ae1636debe963bab43768fe
Paulo Chiliguano <paulo.chiliguano@gmail.com>
2020-11-29 18:18:38 -0500

    Resampling with sinc interpolation

--- LabeledDataEntry.py
+++ LabeledDataEntry.py
@@ -6,7 +6,7 @@ class LabeledDataEntry(object):
 
     def __init__(self, file_path, label):
         self.file_path = file_path
-        self.sample_rate = 24000 #read from file
+        self.sample_rate = 16000 #read from file
 
         if label == 'training-negative-identification':
             self.label = 0
@@ -16,12 +16,12 @@ class LabeledDataEntry(object):
         self.extract_audio_features()
 
     def extract_audio_features(self):
-        audio, sample_rate = librosa.load(self.file_path, sr=self.sample_rate, res_type='kaiser_best')
+        audio, sample_rate = librosa.load(self.file_path, sr=self.sample_rate, res_type='sinc_best')
         self.mfcc = librosa.feature.mfcc(y=audio, sr=sample_rate, n_mfcc=40)
         self.mfccScaled = np.mean(self.mfcc.T, axis=0)
         self.remove_unsuitable_training_file()
 
     def remove_unsuitable_training_file(self):
-        print(self.mfcc.shape[1])
+        print(self.mfcc.shape)
         if self.mfcc.shape[1] != 32:
             os.remove(self.file_path)
