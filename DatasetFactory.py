cc12e263af2731f7bf1990ede4b3a3128af67c7c
Paulo Chiliguano <paulo.chiliguano@gmail.com>
2020-12-04 18:35:50 -0500

    long path to ignore non-audio file

--- DatasetFactory.py
+++ DatasetFactory.py
@@ -7,8 +7,7 @@ def load_wav_files_and_assign_their_labels(wav_files_directory, label_for_files_
     labeled_data_entries = []
 
     for wav_file in wav_files_directory.iterdir():
-        print(wav_file)
-        if wav_file == ".gitignore":
+        if wav_file == "dataset/training/positive-identification/.gitignore":
             continue
         labeled_data_entry = LabeledDataEntry(wav_file, label_for_files_found_in_directory)
         labeled_data_entries.append(labeled_data_entry)
